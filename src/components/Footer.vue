f3d2a4a3e8f643f2493b288f5110301436a4238c
mkeen31 <mkeen31@gmail.com>
2021-08-21 16:12:39 -0400

    work in progress

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,6 @@
             <li><a :href="LINKEDIN" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></li>
             <li><a :href="TWITTER" target="_blank"><i class="fab fa-twitter fa-2x"></i></a></li>
             <li><a :href="INSTAGRAM" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
-            <li><a :href="REDDIT" target="_blank"><i class="fab fa-reddit fa-2x"></i></a></li>
         </ul>
     </footer>
 </template>
@@ -20,7 +19,6 @@ export default {
             LINKEDIN: "https://www.linkedin.com/in/michaeljkeen",
             TWITTER: "https://twitter.com/mkeen31",
             INSTAGRAM: "https://www.instagram.com/mkeen31",
-            REDDIT: "https://www.reddit.com/user/mkeen3",
         }
     }
 }
