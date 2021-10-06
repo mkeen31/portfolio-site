d8db819d8413636008cd77ddb93fcab80843b435
mkeen31 <mkeen31@gmail.com>
2021-10-06 15:28:14 -0400

    Fix overflow problems and add resume link

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,49 +1,54 @@
 <template>
-    <div id="portfolio" class="page-container">
-        <h2>Portfolio</h2>
-        <section>
-            <h4>Education</h4>
-            <div v-for="education in educationData" :key="education.id">
-                <router-link :to="{name: 'education-details', params: { id: education.id }}">
-                    <div class="card">
-                        <img>
-                        <div class="card-container">
-                            <h4><strong>{{ education.degree }}</strong></h4>
-                            <p>{{ education.major }}</p>
-                            <p>{{ education.institution }}</p>
-                        </div>
-                    </div>
-                </router-link>
+    <div class="page-container">
+        <div id="portfolio-content">
+            <h2>Portfolio</h2>
+            <div id="resume-section">
+                <p><a href="#">View Resume</a></p>
             </div>
-        </section>
-        <section class="clear">
-            <h4>Employment Experience</h4>
-            <div v-for="employment in employmentData" :key="employment.id">
-                <router-link :to="{name: 'employment-details', params: {id: employment.id}}">
-                    <div class="card">
-                        <img>
-                        <div class="card-container">
-                            <h4><strong>{{ employment.position }}</strong></h4>
-                            <p>{{ employment.employer }}</p>
+            <section>
+                <h4>Education</h4>
+                <div v-for="education in educationData" :key="education.id">
+                    <router-link :to="{name: 'education-details', params: { id: education.id }}">
+                        <div class="card">
+                            <img>
+                            <div class="card-container">
+                                <h4><strong>{{ education.degree }}</strong></h4>
+                                <p>{{ education.major }}</p>
+                                <p>{{ education.institution }}</p>
+                            </div>
                         </div>
-                    </div>
-                </router-link>
-            </div>
-        </section>
-        <section class="clear">
-            <h4>Project Experience</h4>
-            <div v-for="experience in experienceData" :key="experience.id">
-                <router-link :to="{name: 'experience-details', params: {id: experience.id}}">
-                    <div class="card">
-                        <img>
-                        <div class="card-container">
-                            <h4><strong> {{ experience.name }}</strong></h4>
-                            <p>{{ experience.institution }}</p>
+                    </router-link>
+                </div>
+            </section>
+            <section class="clear">
+                <h4>Employment Experience</h4>
+                <div v-for="employment in employmentData" :key="employment.id">
+                    <router-link :to="{name: 'employment-details', params: {id: employment.id}}">
+                        <div class="card">
+                            <img>
+                            <div class="card-container">
+                                <h4><strong>{{ employment.position }}</strong></h4>
+                                <p>{{ employment.employer }}</p>
+                            </div>
                         </div>
-                    </div>
-                </router-link>
-            </div>
-        </section>
+                    </router-link>
+                </div>
+            </section>
+            <section class="clear">
+                <h4>Project Experience</h4>
+                <div v-for="experience in experienceData" :key="experience.id">
+                    <router-link :to="{name: 'experience-details', params: {id: experience.id}}">
+                        <div class="card">
+                            <img>
+                            <div class="card-container">
+                                <h4><strong> {{ experience.name }}</strong></h4>
+                                <p>{{ experience.institution }}</p>
+                            </div>
+                        </div>
+                    </router-link>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 <script>
