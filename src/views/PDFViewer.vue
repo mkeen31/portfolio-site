6011d9ee36ac77a441b47dd470d9c49fe2a4bb53
Michael Keen <mkeen31@gmail.com>
2021-10-21 21:41:35 -0400

    Add iframe

---
+++ src/views/PDFViewer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page-container">
+        <BackButton/>
+        <div id="pdf-content">
+            <h2>{PDF Name}</h2>
+            <iframe id="pdf" src="../docs/Michael_Keen_Resume.PDF"></iframe>
+        </div>
+    </div>
+</template>
+<script>
+import BackButton from '../components/BackButton.vue';
+import '../styles/App.css';
+export default {
+    components: {
+        BackButton
+    }
+}
+</script>
+<style scoped>
+#pdf {
+    height: 700px;
+    width: 900px;
+    overflow-y: scroll;
+}
+
+#pdf-content {
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    transform: translate(-50%, 0);
+}
+</style>
