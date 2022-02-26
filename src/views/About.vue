0adb21fe8f5f7d2ea5db76ff28b5101cba78affb
mkeen31 <mkeen31@gmail.com>
2022-02-26 14:22:27 -0500

    Update job info

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,10 +13,8 @@
       </p>
       <p>
       I am currently employed as a junior software developer at Accutech Systems Corporation in Muncie, Indiana,
-      a position I have held since January 2022. I am currently a member of the backend team, where we are using 
-      the .NET framework to build REST APIs and quality server-side applications for our clients.Outside of 
-      technology, I enjoy reading about history, philosophy, fantasy, and psychology. I also enjoy spending 
-      time with my girlfriend Gina, my friends, and my family.
+      a position I have held since February 2022. Outside of technology, I enjoy reading about history, philosophy, 
+      fantasy, and psychology. I also enjoy spending time with my girlfriend Gina, my friends, and my family.
       </p>
     </div>
   </div>
